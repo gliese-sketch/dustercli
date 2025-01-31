e21aa297dbfa2d3241f883bc7eb811216c39824c
sweksha2759 <sweksha2759@csmu.ac.in>
2025-01-31 11:02:54 +0000

    fix bugs and add implement core

--- main.js
+++ main.js
@@ -2,25 +2,24 @@ import { intro, outro, log } from "@clack/prompts";
 
 import { readCSV, writeCSV } from "./lib/io.js";
 import prompt from "./lib/prompt.js";
+import core from "./lib/core.js";
 
 async function main() {
   // Start interactive CLI
   intro("DusterCLI app starting");
 
-  // Get user prompt
-  const { input, output, errors } = await prompt();
+  const { input, output, errors: errorsFile } = await prompt();
 
   // Read the file
   const data = readCSV(input);
 
-  log.message("File has been read successfully!");
+  const [clean, errors] = core(data.body);
 
-  // Create clean csv
-  writeCSV(output, data.body);
+  // Create the clean file
+  writeCSV(output, clean);
 
-  log.message("File has been created!");
-
-  // TODO: Create errors.csv
+  // Create errors file
+  writeCSV(errorsFile, errors);
 
   outro("Thanks for using our app!");
 }
