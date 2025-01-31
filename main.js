1ffba58f9263ce6d52217bd5fd48b9b883826473
sweksha2759 <sweksha2759@csmu.ac.in>
2025-01-31 09:00:05 +0000

    yargs support

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv))
-        .option('input', {
-            alias: 'i',
-            type: 'string',
-            description: 'What is your input.csv file?'
-        })
-        .option('output', {
-            alias: 'o',
-            type: 'string',
-            description: 'What will be the ouput.csv file?'
-        })
-        .option('errors', {
-            alias: 'e',
-            type: 'string',
-            description: 'What will be the errors.csv file?'
-        })
-        .version('1.0.0')
-        .alias('v', 'version')
-        .help("h")
-        .alias('h', 'help')
-        .parse()
+// import yargs from './lib/yargs.js'
 
-console.log(argv.input, argv.output, argv.errors)
+import yargs from './lib/yargs.js'
+
+console.log(yargs())
