e45f6925238be4981c3fd7ac8814ed302a7a85d6
sweksha2759 <sweksha2759@csmu.ac.in>
2025-01-31 10:34:31 +0000

    check for valid company name

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -20,3 +20,25 @@ describe("Test cases for Employee Size", () => {
     expect(v.isEmployeeSize("10a")).toBe(false);
   });
 });
+
+describe("Test cases for Company Name", () => {
+  test("check for a valid", () => {
+    expect(v.isCompanyName("e42")).toBe(true);
+  });
+
+  test("check for a valid", () => {
+    expect(v.isCompanyName("Phleebs")).toBe(true);
+  });
+
+  test("check for a valid", () => {
+    expect(v.isCompanyName("wwArena")).toBe(true);
+  });
+
+  test("check for an invalid case", () => {
+    expect(v.isCompanyName("1000")).toBe(false);
+  });
+
+  test("check for an invalid case", () => {
+    expect(v.isCompanyName("www.apple.com")).toBe(false);
+  });
+});
